6312aa343807c8f9f6c75c3ff5ebaf497ce8b5d5
renneee <renpyang@gmail.com>
2017-08-25 06:16:43 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = { Kartell : "5am" };
 
 function updatePlaylist (playlist, artistName, songTitle) {
-  playlist[ artistName ]: "songTitle"
+  playlist[ artistName ]= "songTitle"
   return playlist
 }
