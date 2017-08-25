8884e05f884715fde0cde1843be286407ce434f1
renneee <renpyang@gmail.com>
2017-08-25 06:31:34 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -10,5 +10,5 @@ function updatePlaylist (playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist (playlist, artistName) {
-  return delete playlist.artistName
+  delete playlist.artistName
 }
